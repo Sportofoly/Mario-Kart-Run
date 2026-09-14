884e1e11a4f3d3682e8ae99d806e5acf0fa51d6a
Sportofoly <jonathan.durieux@outlook.com>
2026-09-14 10:27:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    d'afficher le jeu reviendrait à dépendre d'une connexion qu'on n'a pas.
    Une nouvelle version est récupérée en arrière-plan et prend la main à
    l'ouverture suivante, jamais au milieu d'une séance. */
-var CACHE = 'sportofoly-v49c157d0fe';
+var CACHE = 'sportofoly-v72a774c4c6';
 var FICHIERS = ['./', './index.html', './manifest.webmanifest',
                 './icone-180.png', './icone-192.png', './icone-512.png'];
 
